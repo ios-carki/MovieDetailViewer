eedc35d24d6af96c28cdaf6c9d677d6c67846842
carki <94.nogari@gmail.com>
2022-07-28 21:53:15 +0900

    07.28(목) - searchView 구현

--- MovieDetailViewer/WebView/WebViewController.swift
+++ MovieDetailViewer/WebView/WebViewController.swift
@@ -6,20 +6,54 @@
 //
 
 import UIKit
+import WebKit
 
 class WebViewController: UIViewController {
-    @IBOutlet weak var testLabel: UILabel!
+    var destinationURL: String = "https://www.google.com"
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var webView: WKWebView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        testLabel.text = "웹 컨트롤뷰가 자리할 뷰 입니다."
-        testLabel.textAlignment = .center
-        testLabel.font = .boldSystemFont(ofSize: 25)
-        
+        openWebPage(to: destinationURL)
+        searchBar.delegate = self
     }
     
-
+    @IBAction func goBackButtonClicked(_ sender: Any) {
+        if webView.canGoBack {
+            webView.goBack()
+        }
+    }
     
+    
+    @IBAction func reloadButtonClicked(_ sender: Any) {
+        webView.reload()
+    }
+    
+    
+    @IBAction func goForwardButtonClicked(_ sender: Any) {
+        if webView.canGoForward {
+            webView.goForward()
+        }
+    }
+    
+    func openWebPage(to urlStr: String) {
+        guard let url = URL(string: urlStr) else {
+            print("Invalid URL")
+            return
+        }
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
+    
+}
 
+extension WebViewController:UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        openWebPage(to: searchBar.text!)
+    }
 }
